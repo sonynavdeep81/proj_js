318f5f491e5b30bfaed741ecd76171ba463b402d
sonynavdeep81 <sonynavdeep81@gmail.com>
2023-10-02 18:27:36 +0530

    updated 01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+/*
+1. var, let and const are different types of variables. All of them can be hoisted. In hoisting, we move variables 
+and functions to the top before they are initialized. 
+2. During hositing "var" are initialized to "undefined" whereas "let" and "const" are not initialized. Although "let" 
+and "const" can be hoisted like "var" but they cannot be accessed. "var" if accessed before initlization, will 
+return "undefined" whereas "let" and "const" variables remain in temporal dead zone (they will give reference 
+error if accessed in this zone) until they are initialized. Temporal dead zone is the time between when a variable
+is decleared and when it is initialized.
+3. "var" and "let" can be overwritten but const cannot as they cannot be changed.
+4. "let" variables cannot be redeclared like "var"
+5. Both "let" and "const" are block-scoped variables i.e. they can be accessed only within the block whereas "var"
+can be accessed anywhere.
+6. Block-scoped variables "let" and "const" cannot be redeclared.
+7. "var" should never be used.
+*/
+
+/* Hoisting */
+console.log(a); //undefined (means variable declared but not initialized)
+console.log(b); //not defined (means variable isnt even declared)
+console.log(c); //not defined
+var a = "sss";
+let b = "hhh";
+const c = "ggg";
