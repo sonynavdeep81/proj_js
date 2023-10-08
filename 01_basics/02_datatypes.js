7a95a1a4cf4de17e97a67cc98c23865fa2114fe7
sonynavdeep81 <sonynavdeep81@gmail.com>
2023-10-08 18:11:28 +0530

    02_datatypes added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,13 @@
+let a = 10; //number
+let b = "hello"; //string
+let c = true; //boolean
+let d = undefined; //undefined
+let e = null; //null (means empty)
+let f = Symbol("description"); //symbol
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
