2a63fff05310db27f068bfa5b49be32de8556fdd
sonynavdeep81 <sonynavdeep81@gmail.com>
2023-10-22 08:07:17 +0530

    05_truthy_falsy_values added

---
+++ 01_basics/04_template_strings.js
@@ -0,0 +1,17 @@
+let fname = "navdeep";
+let lname = "singh";
+
+console.log("My name is " + fname + " " + lname); //concatenation
+
+console.log(`My name is ${fname} ${lname}`); //string template
+
+console.log(
+  "My name \n\
+is navdeep\n\
+singh"
+);
+
+//string template
+console.log(`My name 
+is navdeep   
+singh`);
---
+++ 01_basics/05_truthy_falsy_values.js
@@ -0,0 +1,10 @@
+/*Falsy values are those that will become false when converted to Boolean*/
+/*Following are 5 falsy values in JS */
+
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean(null));
+console.log(Boolean(NaN));
+console.log(Boolean(undefined));
+
+console.log(Boolean({})); //not falsy
