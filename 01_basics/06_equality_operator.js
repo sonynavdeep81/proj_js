126f5df15fe9e3d118b1f5cf85e670101e521c0b
sonynavdeep81 <sonynavdeep81@gmail.com>
2023-10-24 09:32:51 +0530

    updated

--- 01_basics/06_equality_operator.js
+++ 01_basics/06_equality_operator.js
@@ -1,5 +1,8 @@
-let age = "18";
+let age = '18';
 
-if (age === 18) console.log("Strict operator"); //Does not perform type coercion
-if (age == 18) console.log("Loose operator"); //Performs type coercion ('18' gets converted to 18)
+if (age === 18) console.log('Strict operator'); //Does not perform type coercion
+if (age == 18) console.log('Loose operator'); //Performs type coercion ('18' gets converted to 18)
 //Never use loose operator
+
+if (18 != '18') console.log('Loose inequality');
+if (18 !== '18') console.log('Strict inequality');
